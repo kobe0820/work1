33f5dc3d0bdbe5fb908c8298e01888b62ce79d4c
kobe0820 <145752124+kobe0820@users.noreply.github.com>
2023-10-11 22:14:19 +0800

    Add files via upload

---
+++ Source2.25.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	printf("LLLLLLLLLLLLL\n");
+	printf("L\n");
+	printf("L\n");
+	printf("L\n");
+	printf("L\n");
+	printf("L\n");
+	printf("                    \n");
+	printf(" S          S         \n");
+	printf("  S        S  S        \n");
+	printf("   S      S    S        \n");
+	printf("    S    S      S        \n");
+	printf("     S  S        S       \n");
+	printf("      S           S       \n");
+	printf("                    \n");
+	printf("ZZZ          Z\n");
+	printf("Z  ZZ        Z\n");
+	printf("Z    ZZ      Z\n");
+	printf("Z      ZZ    Z\n");
+	printf("Z        ZZ  Z\n");
+	printf("Z           ZZ\n");
+}
